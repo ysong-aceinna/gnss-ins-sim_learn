a695fd5d040ad63a38ce63147655d9b512eed7d6
ysong-aceinna <ysong@aceinna.com>
2020-01-15 22:01:14 +0800

    add ahrs_ekf2.py, use [q0, q1, q2, q3, wb_x, wb_y, wb_z] as system status vector, use [yaw, pitch, roll] as measurement vector.

--- demo_algorithms/ahrs_ekf2.py
+++ demo_algorithms/ahrs_ekf2.py
@@ -7,7 +7,7 @@ Created on 2020-01-11
 @author: Ocean
 
 选取状态量：[q0, q1, q2, q3, wb_x, wb_y, wb_z]
-选取观测量：[accel]  # 只选accel作为观测量的话还是欠缺了点，因为没有用到mag的信息。但如果把mag也作为观测量，其推导又非常复杂。
+选取观测量：[yaw, pitch, roll]
 """
 
 import numpy as np
@@ -77,7 +77,7 @@ class AHRSEKFTest(object):
             self.qs[i] = self.q
     
         P = np.identity(7) * 0.000001 #init P  [7x7]
-        R = np.identity(3) * 0.1 #init R  [3x3]
+        R = np.identity(3) * 0.0001 #init R  [3x3]
         # init done
 
         for i in range(init_n, n): # i is from init_n+1 to n.
@@ -118,8 +118,8 @@ class AHRSEKFTest(object):
             K = np.dot(np.dot(P_, H.T), np.linalg.inv(S)) #shape [7x3]
 
             # update
-            zk = accel[i] #.reshape(3, 1) # accel measurement.
-            update_states = pred_states + np.dot(K, (zk - np.squeeze(h))) # update states 
+            zk = (self.cal_attitude_by_accel_and_mag(accel[i], mag[i])).reshape(3, 1) #shape [3x1]
+            update_states = pred_states + np.squeeze(np.dot(K, (zk - h))) # update states 
 
             self.q = attitude.quat_normalize(update_states[0:4]) #shape [4x1]
             self.w_bias = update_states[4:7] #shape [3x1]
@@ -278,10 +278,11 @@ class AHRSEKFTest(object):
         h = np.zeros((3, 1))
         q = pred_states[0:4]
 
-        h[0] = 2*q[1]*q[3] - 2*q[0]*q[2]
-        h[1] = 2*q[0]*q[1] + 2*q[2]*q[3]
-        h[2] = q[0]*q[0] - q[1]*q[1] - q[2]*q[2] + q[3]*q[3]
-        return -h
+        # [yaw, ptich, roll]
+        h[0] = math.atan2(2*(q[1]*q[2] + q[0]*q[3]), q[0]*q[0] + q[1]*q[1] - q[2]*q[2] - q[3]*q[3])
+        h[1] = -math.asin(2*(q[1]*q[3] - q[0]*q[2]))
+        h[2] = math.atan2(2*(q[2]*q[3] + q[0]*q[1]), q[0]*q[0] - q[1]*q[1] - q[2]*q[2] + q[3]*q[3])
+        return h
 
     def update_H(self, pred_states):
         '''
@@ -292,24 +293,36 @@ class AHRSEKFTest(object):
         Returns:
             H, the Jacobian Matrix of hk.
         '''
-        # H = np.zeros((3, 4))
-        H = np.zeros((3, 7))
         q = pred_states[0:4]
 
-        H[0][0] = -q[2]
-        H[0][1] =  q[3]
-        H[0][2] = -q[0]
-        H[0][3] =  q[1]
-        H[1][0] =  q[1]
-        H[1][1] =  q[0]
-        H[1][2] =  q[3]
-        H[1][3] =  q[2]
-        H[2][0] =  q[0]
-        H[2][1] = -q[1]
-        H[2][2] = -q[2]
-        H[2][3] =  q[3]
+        # yaw = arctan2(yA, yB)
+        yA = 2*(q[1]*q[2] + q[0]*q[3])
+        yB = q[0]*q[0] + q[1]*q[1] - q[2]*q[2] - q[3]*q[3]
+        yC = yA/yB
 
-        return 2*H
+        # pitch = -arcsin(pA)
+        pA = 2*(q[1]*q[3] - q[0]*q[2])
+
+        # roll = arctan2(rA, rB)
+        rA = 2*(q[2]*q[3] + q[0]*q[1])
+        rB = q[0]*q[0] - q[1]*q[1] - q[2]*q[2] + q[3]*q[3]
+        rC = rA/rB
+
+        H = np.zeros((3, 7))
+        H[0][0] = (1 / (1 + yC * yC)) * ((2 * q[3] * yB) - (yA * 2 * q[0])) / (yB * yB)
+        H[0][1] = (1 / (1 + yC * yC)) * ((2 * q[2] * yB) - (yA * 2 * q[1])) / (yB * yB)
+        H[0][2] = (1 / (1 + yC * yC)) * ((2 * q[1] * yB) + (yA * 2 * q[2])) / (yB * yB)
+        H[0][3] = (1 / (1 + yC * yC)) * ((2 * q[0] * yB) + (yA * 2 * q[3])) / (yB * yB)
+        H[1][0] = (1 / math.sqrt(1 - pA * pA)) * (2 * q[2])
+        H[1][1] = -(1 / math.sqrt(1 - pA * pA)) * (2 * q[3])
+        H[1][2] = (1 / math.sqrt(1 - pA * pA)) * (2 * q[0])
+        H[1][3] = -(1 / math.sqrt(1 - pA * pA)) * (2 * q[1])
+        H[2][0] = (1 / (1 + rC * rC)) * ((2 * q[1] * rB) - (rA * 2 * q[0])) / (rB * rB)
+        H[2][1] = (1 / (1 + rC * rC)) * ((2 * q[0] * rB) + (rA * 2 * q[1])) / (rB * rB)
+        H[2][2] = (1 / (1 + rC * rC)) * ((2 * q[3] * rB) + (rA * 2 * q[2])) / (rB * rB)
+        H[2][3] = (1 / (1 + rC * rC)) * ((2 * q[2] * rB) - (rA * 2 * q[3])) / (rB * rB)
+
+        return H
 
     def update_Q(self, q, wb, t, bi, arw):
         '''
